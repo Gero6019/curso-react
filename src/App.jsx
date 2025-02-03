8fa74c61cc23a4c0e672a2f6996256504ec4e789
Usuario <usuario@email.com>
2025-02-03 10:09:40 -0300

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import { Logos } from './components/Logos'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(10)
 
   return (
     <>
